2056a5479478fa3cd40ec69f27b1a008894303b4
lovelynewlife <newlife_1114752@163.com>
2022-09-22 22:01:29 +0800

    test: add init test and test util data structures.

--- src/skiplist_test.cpp
+++ src/skiplist_test.cpp
@@ -3,7 +3,55 @@
 //
 
 #include "gtest/gtest.h"
+#include <string>
+#include "skiplist.hpp"
 
-TEST(test_test, greeting){
+TEST(exp_test, greeting) {
     std::cout<<"Hello world!\n";
+}
+
+struct test_struct{
+  int int_value{};
+  double double_value{};
+  std::string string_value;
+};
+
+class test_class{
+ private:
+  int int_value{};
+  double double_value{};
+  std::string string_value;
+ public:
+  void setInt(int v) {
+    this->int_value = v;
+  }
+
+  int getInt() const {
+    return this->int_value;
+  }
+
+  void setDouble(double v) {
+    this->double_value = v;
+  }
+
+  double getDouble() const {
+    return this->double_value;
+  }
+
+  void setString(const std::string& v) {
+    this->string_value = v;
+  }
+
+  std::string getString() const {
+    return this->string_value;
+  }
+
+};
+
+TEST(skiplist_test, init) {
+  std::cout << "Test skip list init(construct)\n";
+  skiplist<int, int> int2int_list;
+  skiplist<std::string, int> string2int_list;
+  skiplist<int, test_struct> int2struct_list;
+  skiplist<int, int, 24> int2int_list_24;
 }
